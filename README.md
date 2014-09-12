23f3867307a28bab0a73575d46c88579563a250b
Jason Frame <jason@onehackoranother.com>
2014-09-12 22:12:25 +0100

    api overview and some fixes

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # css-units
+
+Value-type representation of common CSS units plus a conversion function.
+
+## Installation
+
+### npm
+
+Get it:
+
+```shell
+npm install css-units
+```
+
+Require it:
+
+```javascript
+var unit = require('css-units');
+```
+
+## API
+
+#### `var v = unit(value, unit)`
+#### `var v = new unit.Value(value, unit)`
+
+#### `v.value`
+
+#### `v.unit`
+
+#### `value.toString()`
+
+#### `value.convertTo(otherUnit)`
+
+#### `unit.convert()`
+
+## Copyright &amp; License
+
+&copy; 2014 Jason Frame [ [@jaz303](http://twitter.com/jaz303) / [jason@onehackoranother.com](mailto:jason@onehackoranother.com) ]
+
+Released under the ISC license.
--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 var UNITS = {
+
+    //
+    // convertible
+
     'in'    : 0,
     'cm'    : 1,
     'pc'    : 2,
@@ -6,28 +10,42 @@ var UNITS = {
     'pt'    : 4,
     'px'    : 5,
     'deg'   : 6,
-    'rad'   : 7
+    'rad'   : 7,
+    's'     : 8,
+    'ms'    : 9,
+
+    //
+    // non-convertible
+
+    '%'     : true,
+    'em'    : true,
+    'ex'    : true
+
 };
+
+var _ = null;
 
 // from libsass
 var CONVERSION = [
-    /*            in         cm         pc         mm         pt         px         deg          rad        */
-    /* in */    [ 1,         2.54,      6,         25.4,      72,        96,        null,        null        ],
-    /* cm */    [ 1.0/2.54,  1,         6.0/2.54,  10,        72.0/2.54, 96.0/2.54, null,        null        ],
-    /* pc */    [ 1.0/6.0,   2.54/6.0,  1,         25.4/6.0,  72.0/6.0,  96.0/6.0,  null,        null        ],
-    /* mm */    [ 1.0/25.4,  1.0/10.0,  6.0/25.4,  1,         72.0/25.4, 96.0/25.4, null,        null        ],
-    /* pt */    [ 1.0/72.0,  2.54/72.0, 6.0/72.0,  25.4/72.0, 1,         96.0/72.0, null,        null        ],
-    /* px */    [ 1.0/96.0,  2.54/96.0, 6.0/96.0,  25.4/96.0, 72.0/96.0, 1,         null,        null        ],
-    /* deg */   [ null,      null,      null,      null,      null,      null,      1,           Math.PI/180 ],
-    /* rad */   [ null,      null,      null,      null,      null,      null,      180/Math.PI, 1           ],
+    /*            in         cm         pc         mm         pt         px         deg          rad            s           ms      */
+    /* in */    [ 1,         2.54,      6,         25.4,      72,        96,        _,           _,             _,          _       ],
+    /* cm */    [ 1.0/2.54,  1,         6.0/2.54,  10,        72.0/2.54, 96.0/2.54, _,           _,             _,          _       ],
+    /* pc */    [ 1.0/6.0,   2.54/6.0,  1,         25.4/6.0,  72.0/6.0,  96.0/6.0,  _,           _,             _,          _       ],
+    /* mm */    [ 1.0/25.4,  1.0/10.0,  6.0/25.4,  1,         72.0/25.4, 96.0/25.4, _,           _,             _,          _       ],
+    /* pt */    [ 1.0/72.0,  2.54/72.0, 6.0/72.0,  25.4/72.0, 1,         96.0/72.0, _,           _,             _,          _       ],
+    /* px */    [ 1.0/96.0,  2.54/96.0, 6.0/96.0,  25.4/96.0, 72.0/96.0, 1,         _,           _,             _,          _       ],
+    /* deg */   [ _,         _,         _,         _,         _,         _,         1,           Math.PI/180,   _,          _       ],
+    /* rad */   [ _,         _,         _,         _,         _,         _,         180/Math.PI, 1,             _,          _       ],
+    /* s */     [ _,         _,         _,         _,         _,         _,         _,           _,             1,          1000    ],
+    /* ms */    [ _,         _,         _,         _,         _,         _,         _,           _,             0.001,      1       ]
 ];
 
 function convert(value, from, to) {
     
     var ixf = UNITS[from], ixt = UNITS[to];
 
-    if (ixf === void 0) throw new Error("invalid unit: " + from);
-    if (ixt === void 0) throw new Error("invalid unit: " + to);
+    if (typeof ixf !== 'number') throw new Error("invalid unit: " + from);
+    if (typeof ixf !== 'number') throw new Error("invalid unit: " + to);
 
     var factor = CONVERSION[ixf][ixt];
     if (factor !== null) {
@@ -39,8 +57,14 @@ function convert(value, from, to) {
 }
 
 function Value(value, unit) {
+    
+    if (!(unit in UNITS)) {
+        throw new Error("invalid units: " + unit);
+    }
+
     this.value = value;
     this.unit = unit;
+
 }
 
 Value.prototype.toString = function() {
